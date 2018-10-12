a271a8e7ecd6ad61c13195a6f7048808a833b6a8
Alaric Senat <asenat@student.42.fr>
2018-10-12 18:48:47 +0200

    Adding otool to tests

--- DiffTest.mk
+++ DiffTest.mk
@@ -1,4 +1,4 @@
-FAIL_DIR	:= fails
+FAIL_DIRS	:= fails fails/nm fails/otool
 DIFF		:= diff
 BIN_DIR		:= /bin /usr/bin /usr/sbin /usr/libexec
 LIB_DIR		:= /usr/lib
@@ -6,18 +6,28 @@ LIB_DIR		:= /usr/lib
 BINS		:= $(shell find $(BIN_DIR) -type f 2>&-)
 LIBS		:= $(shell find $(LIB_DIR) -type f \( -iname '*.dylib' -or -iname '*.so' -or -iname '*.a' \))
 
+NM_BINS		:= $(addprefix nm\ , $(BINS))
+NM_LIBS		:= $(addprefix nm\ , $(LIBS))
+OTOOL_BINS	:= $(addprefix otool\ , $(BINS))
+OTOOL_LIBS	:= $(addprefix otool\ , $(LIBS))
+
 .PHONY: test_all
-test_all: test_bin test_libs
+test_all: test_nm_bin test_nm_libs
 
-.PHONY: test_bin
-test_bin: all $(FAIL_DIR) $(BINS)
+.PHONY: test_nm_bin
+test_nm_bin: all $(FAIL_DIRS) $(NM_BINS)
 
-.PHONY: test_libs
-test_libs: all $(FAIL_DIR) $(LIBS)
+.PHONY: test_nm_libs
+test_nm_libs: all $(FAIL_DIRS) $(NM_LIBS)
 
-.PHONY: $(BINS) $(LIBS)
-$(BINS) $(LIBS):
-	$(eval DIFF_R := $(shell ./DiffTest.sh "$@"; echo $$?))
+.PHONY: test_otool_bin
+test_otool_bin: all $(FAIL_DIRS) $(OTOOL_BINS)
+
+.PHONY: test_otool_libs
+test_otool_libs: all $(FAIL_DIRS) $(OTOOL_LIBS)
+
+nm\ /%:
+	$(eval DIFF_R := $(shell ./DiffTest.sh nm "" "/$*"; echo $$?))
 	@if [ $(DIFF_R) -gt 0 ]; \
 	then \
 		$(ECHO) "["$(RED)fail$(RESET)"] -" "$@" 1>&2; \
@@ -25,5 +35,14 @@ $(BINS) $(LIBS):
 		$(ECHO) "["$(GREEN)Ok$(RESET)"] -" "$@"; \
 	fi
 
-$(FAIL_DIR):
+otool\ /%:
+	$(eval DIFF_R := $(shell ./DiffTest.sh otool "-t" "/$*"; echo $$?))
+	@if [ $(DIFF_R) -gt 0 ]; \
+	then \
+		$(ECHO) "["$(RED)fail$(RESET)"] -" "$@" 1>&2; \
+	else \
+		$(ECHO) "["$(GREEN)Ok$(RESET)"] -" "$@"; \
+	fi
+
+$(FAIL_DIRS):
 	mkdir $@
--- DiffTest.sh
+++ DiffTest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DIFF=$(diff <(nm "$1" 2>&-) <(./ft_nm "$1" 2>&-))
+DIFF=$(diff <("$1" $2 "$3" 2>&-) <(./ft_"$1" $2 "$3" 2>&-))
 if [ "$DIFF" != "" ]
 then
-	FNAME=$(basename "$1")
-	echo "$1" > fails/"$FNAME".diff
-	echo "$DIFF" >> fails/"$FNAME".diff
+	FNAME=$(basename "$3")
+	echo "$1" > fails/"$1"/"$FNAME".diff
+	echo "$DIFF" >> fails/"$1"/"$FNAME".diff
 	exit 1
 fi
 exit 0
